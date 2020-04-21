61b1bb3f25a1c227855a59aede6f73b576affffa
techandsec <61643810+techandsec@users.noreply.github.com>
2020-04-21 00:54:45 -0400

    Added short name for disqus

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
         {
             resolve: `gatsby-plugin-disqus`,
             options: {
-              shortname: `mysiteNovela`
+              shortname: `techandsec-com`
             }
           },
         {
