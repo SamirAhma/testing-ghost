546aa0626ea93387cae1c81691687464b5d19691
SamirAhma <ahmaddin1000@gmail.com>
2020-04-21 18:07:11 +0800

    test code

--- src/gatsby-theme-try-ghost/templates/post.js
+++ src/gatsby-theme-try-ghost/templates/post.js
@@ -175,7 +175,7 @@ const Post = ({ data, location, pageContext }) => {
                                 </section>
 
                                 {/* <Comments id={post.id}/> */}
-                                <Disqus />
+                                <Disqus id={post.id} />
                             </article>
                         </div>
                     </Layout>
